5e6453a1b7556ea228d4be49e62d309f718bd971
Ronillo Apas Jr <jexos26@gmail.com>
2020-04-17 11:26:34 +0800

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-theme-material-ui`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/data/`,
+      },
+    },
+    `gatsby-plugin-sass`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
